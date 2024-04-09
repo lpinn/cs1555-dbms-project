10bbd858c15ef259a056559160da5e73001584db
lpinn <71280609+lpinn@users.noreply.github.com>
2024-04-09 09:51:41 -0400

    Update operations.sql

--- operations.sql
+++ operations.sql
@@ -1,3 +1,111 @@
+/* 1. */
+CREATE PROCEDURE create_account(
+    IN username VARCHAR(30),
+    IN passkey VARCHAR(30),
+    IN role VARCHAR(12))
+LANGUAGE plpgsql
+AS $$
+    BEGIN
+        INSERT INTO olympic_schema.ACCOUNT(account_id, username, passkey, role, last_login)
+            VALUES(account_id, username, passkey, role, current_timestamp);
+        RAISE NOTICE 'Account added successfully.';
+
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE NOTICE 'Error.';
+    END
+$$;
+
+/* 2 */
+CREATE OR REPLACE PROCEDURE olympic_schema.remove_account(
+    IN aid INTEGER
+)
+LANGUAGE plpgsql
+AS $$
+    DECLARE
+        row_check INTEGER;
+    BEGIN
+        DELETE FROM olympic_schema.account
+        WHERE olympic_schema.account.account_id = aid;
+
+       GET DIAGNOSTICS row_check = ROW_COUNT;
+
+        COMMIT;
+
+        if(row_check = 0) then
+            RAISE NOTICE 'Account with the given account_id does not exist.';
+        else
+            RAISE NOTICE 'Account was deleted successfully.';
+        end if;
+
+    EXCEPTION
+        WHEN foreign_key_violation THEN
+            RAISE EXCEPTION 'Foreign key violation.';
+        WHEN OTHERS THEN
+            RAISE EXCEPTION 'Error - checking: %', SQLERRM;
+    END
+$$;
+
+/* 3. */
+CREATE PROCEDURE add_participant(
+    IN account_id INTEGER,
+    IN first VARCHAR(30),
+    IN middle VARCHAR(30),
+    IN last VARCHAR(30),
+    IN birth_country CHAR(3),
+    IN dob TIMESTAMP,
+    IN gender VARCHAR(1)
+)
+LANGUAGE plpgsql
+AS $$
+    BEGIN
+        INSERT INTO olympic_schema.PARTICIPANT(account, first, middle, last, birth_country, dob, gender)
+            VALUES(account_id, first, middle, last, birth_country, dob, gender);
+        RAISE NOTICE 'Participant added successfully.';
+
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE NOTICE 'Error.';
+    END
+$$;
+
+/* 4. */
+CREATE PROCEDURE olympic_schema.remove_participant(
+    IN pid INTEGER
+)
+LANGUAGE plpgsql
+AS $$
+    DECLARE
+        row_check_participant INTEGER;
+        row_check_account INTEGER;
+    BEGIN
+        WITH participant_removal AS (
+            DELETE FROM olympic_schema.participant
+            WHERE olympic_schema.participant.participant_id = pid
+            RETURNING olympic_schema.participant.account
+        )
+        DELETE FROM olympic_schema.account
+        WHERE olympic_schema.account.account_id = participant_removal.account;
+
+        GET DIAGNOSTICS row_check_account = ROW_COUNT;
+
+        COMMIT;
+
+        if(row_check_participant = 0) then
+            RAISE NOTICE 'Account with the given account_id does not exist.';
+        else
+            RAISE NOTICE 'Account was deleted successfully.';
+        end if;
+
+    EXCEPTION
+        WHEN foreign_key_violation THEN
+            RAISE EXCEPTION 'Foreign key violation.';
+        WHEN OTHERS THEN
+            RAISE EXCEPTION 'Error - checking: %', SQLERRM;
+    END
+$$;
+
+/* 5. */
 CREATE PROCEDURE add_team_member(
     IN team INTEGER,
     IN participant INTEGER)
@@ -14,15 +122,53 @@ AS $$
     END
 $$;
 
-/* ddl */
-create procedure add_participant(IN account_id integer, IN first character varying, IN middle character varying, IN last character varying, IN birth_country character, IN dob timestamp without time zone, IN gender character varying)
-    language plpgsql
-as
-$$
+/* 6. */
+/* 2 */
+CREATE OR REPLACE PROCEDURE olympic_schema.remove_team_member(
+    IN pid INTEGER,
+    IN tid INTEGER
+)
+LANGUAGE plpgsql
+AS $$
+    DECLARE
+        row_check INTEGER;
     BEGIN
-        INSERT INTO olympic_schema.PARTICIPANT(account, first, middle, last, birth_country, dob, gender)
-            VALUES(account_id, first, middle, last, birth_country, dob, gender);
-        RAISE NOTICE 'Participant added successfully.';
+        DELETE FROM olympic_schema.team_members
+        WHERE olympic_schema.team_members.participant = pid
+            AND olympic_schema.team_members.team = tid;
+
+       GET DIAGNOSTICS row_check = ROW_COUNT;
+
+        COMMIT;
+
+        if(row_check = 0) then
+            RAISE NOTICE 'Team member with the given parameters does not exist.';
+        else
+            RAISE NOTICE 'Team member was removed successfully.';
+        end if;
+
+    EXCEPTION
+        WHEN foreign_key_violation THEN
+            RAISE EXCEPTION 'Foreign key violation.';
+        WHEN OTHERS THEN
+            RAISE EXCEPTION 'Error - checking: %', SQLERRM;
+    END
+$$;
+
+/* 7. */
+CREATE PROCEDURE register_team(
+    IN olympiad VARCHAR(30),
+    IN sport INTEGER,
+    IN coach CHAR(3),
+    IN country CHAR(3),
+    IN gender VARCHAR(1)
+)
+LANGUAGE plpgsql
+AS $$
+    BEGIN
+        INSERT INTO olympic_schema.TEAM(olympiad, sport, coach, country, gender)
+            VALUES(olympiad, sport, coach, country, gender);
+        RAISE NOTICE 'Team added successfully.';
 
     EXCEPTION
         WHEN OTHERS THEN
@@ -30,22 +176,23 @@ $$
     END
 $$;
 
-alter procedure add_participant(integer, varchar, varchar, varchar, char, timestamp, varchar) owner to postgres;
-
-create procedure create_account(IN username character varying, IN passkey character varying, IN role character varying)
-    language plpgsql
-as
-$$
+/* 8. */
+CREATE PROCEDURE add_event(
+    IN venue_id VARCHAR(30),
+    IN olympiad_id VARCHAR(30),
+    IN sport_id INTEGER,
+    IN gender VARCHAR(1),
+    IN date TIMESTAMP
+)
+LANGUAGE plpgsql
+AS $$
     BEGIN
-        INSERT INTO olympic_schema.ACCOUNT(username, passkey, role, last_login) VALUES(username, passkey, role, current_timestamp);
-        RAISE NOTICE 'Account added successfully.';
+        INSERT INTO olympic_schema.EVENT(venue, olympiad, sport, gender, date)
+            VALUES(venue_id, olympiad_id, sport_id, gender, date);
+        RAISE NOTICE 'Event added successfully.';
 
     EXCEPTION
         WHEN OTHERS THEN
             RAISE NOTICE 'Error.';
     END
 $$;
-
-alter procedure create_account(varchar, varchar, varchar) owner to postgres;
-
-
