5cd89e1ee81535fdfe34dc1b71794002733c61d3
lpinn <71280609+lpinn@users.noreply.github.com>
2024-04-15 23:36:56 -0400

    Update OlympicDB.java

--- OlympicDB.java
+++ OlympicDB.java
@@ -1,5 +1,11 @@
+import java.sql.*; 
+
 public class OlympicDB{
+    private Connection dbcon; 
+    private String username = "postgres"; 
+    private String password = "CowCow2024"; 
     public static void main(String args[]){
+        
         
     }
 }
